e51ecbbe3ee9bd97bc0b64d8788b3279727436de
CaMiLoBC17 <cbc_777@hotmail.com>
2025-02-09 16:48:15 -0500

    test5

--- main.py
+++ main.py
@@ -5,16 +5,23 @@ import joblib
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "The API is working successfully..."}
 
+
 @app.post("/batch_prediction")
 async def predict(file: UploadFile = File(...)):
+    # Cargar el modelo de predicción
     model = joblib.load("model/tree_model.joblib")
 
+    # Leer el contenido del archivo CSV
     content = await file.read()
     data = pd.read_csv(StringIO(content.decode("utf-8")))
 
+    # Realizar la predicción
     predictions = model.predict(data)
+
+    # Retornar las predicciones
     return {"predictions": list(predictions)}
